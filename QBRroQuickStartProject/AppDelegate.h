c9e860f45546f17d1645f181d2211712b3376942
HongDi Huang <hdhuangh@isoftstone.com>
2018-03-22 10:54:46 +0800

    Initial Commit

---
+++ QBRroQuickStartProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QBRroQuickStartProject
+//
+//  Created by HongDi Huang on 22/03/2018.
+//  Copyright © 2018 HongDi Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QBRroQuickStartProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QBRroQuickStartProject
+//
+//  Created by HongDi Huang on 22/03/2018.
+//  Copyright © 2018 HongDi Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
